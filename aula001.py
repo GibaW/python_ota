902f48f27f97372eeb77e48fe339f2e8433315be
GibaW <gibagw@gmail.com>
2023-10-26 14:26:36 -0300

    tentativa tortoise

--- aula001.py
+++ aula001.py
@@ -1,5 +1,5 @@
 ''' Usar para escrever suas notas jdjdjdjdjk  xxx conflito com a main aqui é a B'''
-
+# linha 2 criada na main
 # Permite escrever um comentário 
 #aqui mudado na branch main
 print(123)  # Na frente
