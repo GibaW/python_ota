df497c44ace8f2a164143d5802b2c21635add7fe
GibaW <gibagw@gmail.com>
2023-10-26 10:42:06 -0300

    outro

--- aula001.py
+++ aula001.py
@@ -1,11 +1,7 @@
-"""
-ccccccccccccc
-"""
-
 ''' Usar para escrever suas notas   ddd'''
 ''' Usar para escrever suas notas jdjdjdjdjk  xxx conflito com a main aqui é a B'''
 
-# Permite escrever um comentário 
+# Permite escrever um comentário na b_teste
 #aqui mudado na branch teste
 print(123)  # Na frente
 # Abaixo
